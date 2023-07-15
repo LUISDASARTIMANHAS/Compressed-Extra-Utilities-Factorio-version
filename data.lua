b5c0be49347350637bb81a1ca162c10b4eff61a2
LUIS DAS ARTIMANHAS <luisaugustodesouza785@gmail.com>
2023-07-15 12:48:56 -0300

    updated

--- data.lua
+++ data.lua
@@ -4,7 +4,7 @@ require("data/compressed_itens.lua")
 -- receitas
 require("data/compressed_recipes.lua")
 require("data/descompressed_recipes.lua")
-require("resources_compressed_recipes.lua")
+require("data/resources_compressed_recipes.lua")
 
 -- presets Basicos do mods
 require("graficos/style.lua")
