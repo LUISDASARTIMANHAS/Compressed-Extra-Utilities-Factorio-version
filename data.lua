3784d7842d84cc6ab02e67cd6f234c68ff8eb0a6
LUIS DAS ARTIMANHAS <luisaugustodesouza785@gmail.com>
2023-07-22 14:05:02 -0300

    gui de coordenadas do jogador

--- data.lua
+++ data.lua
@@ -8,4 +8,5 @@ require("data/resources_compressed_recipes.lua")
 
 -- presets Basicos do mods
 require("graficos/style.lua")
+require("control/cordenadas_gui.lua")
 require("data/compressed_grupos.lua")
