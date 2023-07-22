58652a6b8042035bb88ad46411be59e4b6682327
LUIS DAS ARTIMANHAS <luisaugustodesouza785@gmail.com>
2023-07-22 14:56:10 -0300

    updated

--- control/cordenadas.lua
+++ control/cordenadas.lua
@@ -5,7 +5,7 @@ local gui_coordenadas = require("control/gui_cordenaddas.lua")
 script.on_event(defines.events.on_player_joined_game, function(event)
     local player = game.get_player(event.player_index)
     player.print("Bem-vindo, " .. player.name .. "!")
-    -- Chama a função create_coordinates_gui para criar a GUI das coordenadas
+    -- Call the create_coordinates_gui function to create the GUI for coordinates
     gui_coordenadas.create_coordinates_gui(player)
 end)
 
